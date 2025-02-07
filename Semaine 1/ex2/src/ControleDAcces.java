5f3693faa7f01a725fe79a8dfbcaecd5ca66fa44
juptofex <julien.damilot@gmail.com>
2025-02-07 10:13:58 +0100

    feat: ex2 - completed

--- Semaine 1/ex2/src/ControleDAcces.java
+++ Semaine 1/ex2/src/ControleDAcces.java
@@ -1,27 +1,37 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class ControleDAcces {
 
-	private
+	private Map<Badge, Employe> badgeEmployeMap;
+	private Set<Employe> employeScanne;
 	
 	public ControleDAcces(){
-		
+		badgeEmployeMap = new HashMap<Badge, Employe>();
+		employeScanne = new HashSet<Employe>();
 	}
 	
 	// associe le badge � un employ�
 	public void donnerBadge (Badge b, Employe e){
+		badgeEmployeMap.put(b,  e);
 	}
 	
 	// met � jour les employ�s pr�sents dans le batiment
 	public void entrerBatiment (Badge b){
+		employeScanne.add(badgeEmployeMap.get(b));
 	}
 
 	// met � jour les employ�s pr�sents dans le batiment
 	public void sortirBatiment (Badge b){
-
+		employeScanne.remove(badgeEmployeMap.get(b));
 	}
 	
 	// renvoie vrai si l'employ� est dans le batiment, faux sinon
 	public boolean estDansBatiment (Employe e){
+		if (employeScanne.contains(e))
+			return true;
 		return false;
 	}
-
 }
