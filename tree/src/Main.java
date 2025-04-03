f6c4867d8d357a691ad64276550a24c87feffc1b
juptofex <julien.damilot@gmail.com>
2025-04-03 12:34:35 +0200

    feat: exoRecursivite 1.1 -- 1.5

---
+++ tree/src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+  public static void main(String[] args){
+    Tree l1 = new Tree(1);
+    Tree l3 = new Tree(3);
+    Tree l5 = new Tree(5);
+    Tree l7 = new Tree(7);
+    
+    Tree t2 = new Tree(2, new Tree[]{l1, l3});
+    Tree t6 = new Tree(6, new Tree[]{l7});
+    
+    Tree t4 = new Tree(4, new Tree[]{t2, l5, t6});
+    Tree t12 = new Tree(12, new Tree[]{t2, l5, t6});
+
+    System.out.println(Trees.nbrLeaves(t4));
+    
+    
+    Tree[] ls = Trees.flattenLeaves(t4);
+    System.out.println("Les " + ls.length + " feuilles");
+    int i = 0;
+    while(i != ls.length) {
+      System.out.println(ls[i].getValue());
+      i++;
+    }
+
+    System.out.println("Path V1");    
+    Trees.pathV1(l7);
+
+    System.out.println("Path V2");    
+    Trees.pathV2(l7);
+
+    System.out.println("NbrNoeuds");
+    System.out.println(Trees.nbrNode(t4));
+
+    System.out.println("Min");
+    System.out.println(Trees.min(t4));
+
+    System.out.println("Sum");
+    System.out.println(Trees.sum(t4));
+
+    System.out.println("equals true");
+    System.out.println(Trees.equals(t4, t4));
+    System.out.println("equals false");
+    System.out.println(Trees.equals(t4, t6));
+    System.out.println("equals false");
+    System.out.println(Trees.equals(t4, t12));
+
+    System.out.println("Depth");
+    System.out.println(Trees.depth(l1));
+  }
+}
---
+++ tree/src/Trees.java
@@ -0,0 +1,175 @@
+public class Trees {
+
+	// *******************************************************
+	// Un premier exemple: le nombre de feuilles d'un arbre
+	// *******************************************************
+	public static int nbrLeaves(Tree t) {
+		int r;
+		if (t.isLeaf()) {
+			r = 1;
+		} else {
+			r = 0;
+			for (Tree child : t) {
+				r += nbrLeaves(child);
+			}
+		}
+		return r;
+	}
+
+	// *******************************************************
+	// Un deuxième exemple: aplanir un arbre
+	// *******************************************************
+	public static Tree[] flattenLeaves(Tree t) {
+		int nl = nbrLeaves(t);
+		Tree[] r = new Tree[nl];
+		flattenLeaves(t, r, 0);
+		return r;
+	}
+
+	private static int flattenLeaves(Tree t, Tree[] a, int idx) {
+		int r;
+		if (t.isLeaf()) {
+			a[idx] = t;
+			r = 1;
+		} else {
+			r = 0;
+			for (Tree child : t) {
+				r += flattenLeaves(child, a, idx + r);
+			}
+		}
+		return r;
+	}
+
+	// *******************************************************
+	// Un troisième exemple:
+	// tous les algorithmes ne sont pas récursifs
+	// *******************************************************
+	public static void pathV1(Tree t) {
+		System.out.println(t.getValue());
+		if (t.getParent() != null) {
+			pathV1(t.getParent());
+		}
+	}
+
+	public static void pathV2(Tree t) {
+		while (t != null) {
+			System.out.println(t.getValue());
+			t = t.getParent();
+		}
+	}
+
+	// *******************************************************
+	// Exercices 1
+	// *******************************************************
+
+	// 1.1)
+	public static int nbrNode(Tree t) {
+		int r=1;
+		if (t.isLeaf()) {
+			return r;
+		}
+		for (Tree child: t) {
+			r+=nbrNode(child);
+		}
+		return r;
+	}
+
+	// 1.2)
+	public static int min(Tree t) {
+		int v=t.getValue();
+		if (t.isLeaf()) {
+			return t.getValue();
+		}
+		for (Tree child: t) {
+			int temp=min(child);
+			if (v>temp)
+				v=temp;
+		}
+		return v;
+	}
+
+	// 1.3)
+	public static int sum(Tree t) {
+		int v=t.getValue();
+		if (t.isLeaf()) {
+			return t.getValue();
+		}
+		for (Tree child: t) {
+			v+=sum(child);
+		}
+		return v;
+	}
+
+	// 1.4)
+	public static boolean equals(Tree t1, Tree t2) {
+		if (t1.getValue()!= t2.getValue()) return false;
+		int n1=t1.nbrChildren();
+		int n2=t2.nbrChildren();
+		if (n1!=n2) return false;
+		for (int i = 0; i < n1; i++) {
+			if (!equals(t1.getChildren()[i], t2.getChildren()[i]))
+				return false;
+		}
+		return true;
+	}
+
+	// 1.5)
+	public static int depth(Tree n) {
+		int i=0;
+		if (n.getParent()==null)
+			return i;
+		i = depth(n.getParent()) + 1;
+		return i;
+	}
+
+	// 1.6)
+	public static boolean sameOne(Tree n1, Tree n2) {
+		return true;
+	}
+
+	// 1.7)
+	public static void dfsPrint(Tree t) {
+	}
+
+	// 1.8)
+	public static void bfsPrint(Tree t) {
+	}
+
+	// *******************************************************
+	// Exercices 2
+	// *******************************************************
+
+	// 2.1)
+	static void printPathV1(Tree node) {
+	}
+
+	// 2.2)
+	static void printPathV2(Tree node) {
+	}
+
+	// 2.3
+	static void printPathV3(Tree t, int v) {
+	}
+
+	// *******************************************************
+	// Exercices 3
+	// *******************************************************
+
+	// 3.1
+	public static int[][] toArray(Tree t) {
+		return null;
+	}
+
+	// 3.2
+	public static Tree toTree(int[][] t) {
+		return null;
+	}
+
+	// *******************************************************
+	// Exercices 4
+	// *******************************************************
+
+	public static Tree lca(Tree n1, Tree n2) {
+		return null;
+	}
+}
